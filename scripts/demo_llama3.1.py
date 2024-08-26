71b37c2d92dc7f17c2134eff2fda1a3ef9bd7415
vaps_EVO <vapsok299@gmail.com>
2024-08-27 04:37:33 +0900

    20240827

--- scripts/demo_llama3.1.py
+++ scripts/demo_llama3.1.py
@@ -1,13 +1,8 @@
 from llama_cpp import Llama
-# プロンプトを記入
-prompt = """[INST] <<SYS>>
-You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.Please ensure that your responses are socially unbiased and positive in nature.If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct.If you don't know the answer to a question, please don't share false information.
-<</SYS>>
-Write a story about llamas.Please answer in Japanese.[/INST]"""
-# ダウンロードしたModelをセット.
+
 llm = Llama(model_path="../models/Meta-Llama-3-8B-Instruct.Q6_K.gguf", n_gpu_layers=10)
-# 生成実行
-output = llm(
-    prompt,max_tokens=500,stop=["System:", "User:", "Assistant:"],echo=True,
-)
-print(output)
+
+
+out=llm("Q:朝は四本足、昼は二本足、夕は三本足。この生き物は何か？ A: ")
+
+print(out)
